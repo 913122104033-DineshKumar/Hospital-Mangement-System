62f83447db05830d9338927c4698986f01257c52
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2025-03-04 20:22:32 +0530

    Initial Commits

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             child: Row(
-              
             ),
           ),
         ],
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,4 @@
 import 'package:go_router/go_router.dart';
-import 'package:hospital_management_system/main.dart';
 
 class AppRoutes {
   final GoRouter router = GoRouter(
