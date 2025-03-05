5050fa063a7cb4dd0343e481b0a9f4b2843cd26a
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2025-03-05 15:26:13 +0530

    Added Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_management_system/widgets/card_layout.dart';
 import 'package:hospital_management_system/widgets/home_carousel.dart';
 import 'package:hospital_management_system/widgets/navbar.dart';
 
@@ -18,7 +19,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +41,146 @@ class _HomeScreenState extends State<HomeScreen> {
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: HomeCarousel(),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        cardLayout(
+                          Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Health Check Ups',
+                                  style: textFieldStyle(
+                                    Color(0xFF2ECC71),
+                                    FontWeight.bold,
+                                    18.0,
+                                  ),
+                                ),
+                                SizedBox(height: 3.0),
+                                Text(
+                                  'Besides providing world class clinical lab services, Labaid Diagnostic Centre houses a pool of doctors of different medical specialty to serve the ailing patients as outpatients. They are all reputed and respected in their medical specialty for outstanding clinical management',
+                                  style: textFieldStyle(
+                                    Colors.black,
+                                    FontWeight.normal,
+                                    14.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        cardLayout(
+                          Container(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.check_circle,
+                                color: Color(0xFF2ECC71),
+                              ),
+                              title: Text(
+                                'EMERGENCY CASE',
+                                style: textFieldStyle(
+                                  Color(0xFF2ECC71),
+                                  FontWeight.bold,
+                                  18.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Unless they are blinded by lust and do not gnaw, they are in fault who forsake their duties',
+                                style: textFieldStyle(
+                                  Colors.black,
+                                  FontWeight.normal,
+                                  14.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        cardLayout(
+                          Container(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.check_circle,
+                                color: Color(0xFF2ECC71),
+                              ),
+                              title: Text(
+                                'QUALIFIED DOCTORS',
+                                style: textFieldStyle(
+                                  Color(0xFF2ECC71),
+                                  FontWeight.bold,
+                                  18.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Unless they are blinded by lust and do not gnaw, they are in fault who forsake their duties',
+                                style: textFieldStyle(
+                                  Colors.black,
+                                  FontWeight.normal,
+                                  14.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        cardLayout(
+                          Container(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.check_circle,
+                                color: Color(0xFF2ECC71),
+                              ),
+                              title: Text(
+                                'ONLINE APPOINTMENT',
+                                style: textFieldStyle(
+                                  Color(0xFF2ECC71),
+                                  FontWeight.bold,
+                                  18.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Unless they are blinded by lust and do not gnaw, they are in fault who forsake their duties',
+                                style: textFieldStyle(
+                                  Colors.black,
+                                  FontWeight.normal,
+                                  14.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        cardLayout(
+                          Container(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.check_circle,
+                                color: Color(0xFF2ECC71),
+                              ),
+                              title: Text(
+                                'FREE MEDICAL COUNSELING',
+                                style: textFieldStyle(
+                                  Color(0xFF2ECC71),
+                                  FontWeight.bold,
+                                  18.0,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Unless they are blinded by lust and do not gnaw, they are in fault who forsake their duties',
+                                style: textFieldStyle(
+                                  Colors.black,
+                                  FontWeight.normal,
+                                  14.0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
